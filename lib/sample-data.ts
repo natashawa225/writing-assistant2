156d41e368a81422ae8826b317b16282ecf5194e
natashawa225 <natasha.atmadja2@gmail.com>
2026-03-08 20:55:37 +0800

    prompt updated

--- lib/sample-data.ts
+++ lib/sample-data.ts
@@ -2,13 +2,32 @@ export const samplePrompts = [
     {
       id: 1,
       prompt:
-      
-        "In many countries, university graduates struggle to find jobs in their field of study. Some people believe that the main purpose of university education should be to prepare students for employment. Others argue that universities should focus on academic knowledge, even if it is not directly useful for a job. To what extent do you agree or disagree that the main role of university education is to prepare students for employment?"
+        "Should university education be free for all students?"
         },
     {
       id: 2,
       prompt:
-      "Cities around the world are facing problems of traffic congestion and air pollution. Some people think governments should invest more in improving public transport, while others believe individuals should be free to use their private cars without restrictions. To what extent do you agree or disagree that governments should prioritize investment in public transport rather than allowing unrestricted private car use?"
+      "Should social media platforms be responsible for controlling misinformation?"
+    },
+    {
+      id: 3,
+      prompt:
+      "Should the use of artificial intelligence in education be limited?"
+    },
+    {
+      id: 4,
+      prompt:
+      "Should students be required to wear uniforms at university?"
+    },
+    {
+      id: 5,
+      prompt:
+      "Should the government restrict the use of private cars in large cities?"
+    },
+    {
+      id: 6,
+      prompt:
+      "Should companies be required to allow employees to work from home?"
     },
   ]
   
